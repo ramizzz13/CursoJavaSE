afc4ca15ea2267afe2647c0b0ccaaffaa845b5bb
Ramiz Perez <ramizzz13@gmail.com>
2021-09-10 17:03:05 -0500

    Variables en Java

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Assignamos un valor a una variable
+        speed = 10;
+
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Ramiz Perez";
+        System.out.println(employeeName);
+    }
+}
