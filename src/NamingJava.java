9c5866bc149116b2e7c9bba828993e6fe6afc766
Ramiz Perez <ramizzz13@gmail.com>
2021-09-10 17:27:12 -0500

    Convención de Nombres en Java

---
+++ src/NamingJava.java
@@ -0,0 +1,23 @@
+import org.omg.CORBA.WStringSeqHelper;
+
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 132213;
+        int celPhone = 111122;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $contryName = "Mexico";
+        String _backgroundColor = "Green";
+        String currency$ = "MNX";
+        String backgroung_color = "red";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+
+    }
+}
