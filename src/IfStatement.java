916cb901a9d4d9d7c4db10aac5f531da96dacb91
Ramiz Perez <ramizzz13@gmail.com>
2021-09-17 14:15:41 -0500

    Sentencia IF

---
+++ src/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluethoothEnabled = true;
+        int fileSended = 3;
+
+        if (isBluethoothEnabled){
+            //Send file
+            fileSended++;
+            System.out.println("Archivo enviado");
+        }
+    }
+}
