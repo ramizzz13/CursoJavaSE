d1e65a103414be72aa6d64743bb228a87e028b3d
Ramiz Perez <ramizzz13@gmail.com>
2021-09-17 16:37:06 -0500

    Javadoc en funciones

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,8 +1,6 @@
 public class Funciones {
     public static void main(String[] args) {
         double y = 3;
-
-
         //Area de un circulo
         //pi *r2
         circleArea(y);
@@ -15,22 +13,44 @@ public class Funciones {
         //(4/3)*PI*r3
         sphereVolume(y);
 
-        System.out.println("Pesos a Dolares: " +  converToDollar(200, "MNX"));
+        System.out.println("Pesos a Dolares: " +  convertToDollar(200, "MNX"));
+
     }
 
+    /**
+     * Descripción: Función que calcula el area de un circulo.
+     * @param r Valor del radio que se utiliza para el calculo.
+     * @return Regresa el valor del area del circulo.
+     */
     public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Descripción: Función de calcula el area de una esfera.
+     * @param r Valor del radio que se utilizara para el calculo.
+     * @return Regresa el valor del area de la esfera.
+     */
     public static  double sphereArea(double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Descripción: Función que calcula el volumen de un esfera.
+     * @param r Valor del radio que se utilizara para el calculo.
+     * @return Regresa el valor del volumen de la esfera.
+     */
     public static double sphereVolume(double r){
         return (4/3) * Math.PI * Math.pow(r,3);
     }
 
-    public static  double converToDollar(double quantity, String currency){
+    /**
+     * Descripción: Función que especificando su moneda convierte una cantidad de dinero a dolares.
+     * @param quantity Cantidad de dinero.
+     * @param currency Tipo de moneda: solo acepta MNX or COP.
+     * @return quantity Devuelve la cantidad actualizada en Dolares.
+     */
+    public static  double convertToDollar(double quantity, String currency){
         //MXN COP
         switch (currency){
             case "MNX":
