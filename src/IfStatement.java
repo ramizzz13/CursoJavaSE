d7bd253ed619b0c3340e6aba1258e20f00513336
Ramiz Perez <ramizzz13@gmail.com>
2021-09-17 14:21:36 -0500

    Alcance de las variables y Sentencia ELSE

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,12 +1,18 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluethoothEnabled = true;
+        boolean isBluethoothEnabled = false;
         int fileSended = 3;
 
         if (isBluethoothEnabled){
             //Send file
             fileSended++;
             System.out.println("Archivo enviado");
+        } else {
+            fileSended--;
+            System.out.println("Enciende tu Bluethooth para iniciar transferencia");
         }
+
+        System.out.println(isBluethoothEnabled);
+        System.out.println(fileSended);
     }
 }
