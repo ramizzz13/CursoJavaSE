e951d3fe93df844581569ecb9ae089187912d87f
Ramiz Perez <ramizzz13@gmail.com>
2021-09-19 16:54:38 -0500

    Declarando arreglos

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String [] androidVersions = new String[17];
+        String days [] = new String[7];
+
+        String [][] cities = new String[4][2];
+
+        int [][][] numbers = new int[2][2][2];
+        int [][][][] numbers4 = new int[2][2][2][2];
+
+    }
+}
